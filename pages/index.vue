8e59cc871f14bf9832d0b3539d3db7cd11f6ccab
HugoBzn <hugobazan1499@gmail.com>
2022-09-05 17:27:17 -0500

    SmallCardDisplay with data update

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="container">
     <Hero />
-    <LargeCardDisplay 
-    v-for="cardInfo in largeCardInfo"
-    :key="cardInfo.id"
-    :cardsSection="cardInfo"
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
     />
   </div>
 </template>
 
 <script>
-import { largeCardSections } from "@/assets/data.js";
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
 export default {
   data() {
     return {
       largeCardInfo: largeCardSections,
-    }
-  }
+      smallCardSections,
+    };
+  },
 };
 </script>
